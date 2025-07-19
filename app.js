97232f18bd2a0bf7d6791cf5d35ff5d5786975c7
kartikpandey0007 <kartikkpandey321@gmail.com>
2025-07-19 15:47:18 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,18 +1,659 @@
-const heading = React.createElement("h1", {id:"heading"},"Hello React")
+import React from "react";0
+
+import ReactDOM from "react-dom/client";
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className = "logo" src = "https://videos.openai.com/vg-assets/assets%2Ftask_01k0a7sa20f289xeyv86zc483q%2F1752690475_img_1.webp?st=2025-07-16T17%3A27%3A53Z&se=2025-07-22T18%3A27%3A53Z&sks=b&skt=2025-07-16T17%3A27%3A53Z&ske=2025-07-22T18%3A27%3A53Z&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skoid=3d249c53-07fa-4ba4-9b65-0bf8eb4ea46a&skv=2019-02-02&sv=2018-11-09&sr=b&sp=r&spr=https%2Chttp&sig=M%2Fq1LTrSMloESb8dYJ6P8wNGZ2VHXrEdvDhFDDYJKxA%3D&az=oaivgprodscus" /> 
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const RestaurentCard = ({ resData }) => {
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.cloudinaryImageId
+        }
+      />
+      <h3>{resData.name}</h3>
+      <h4>{resData.cuisines.join(", ")}</h4>
+      <h4>{resData.costForTwo}</h4>
+      <h4>{resData.sla.deliveryTime} mins</h4>
+    </div>
+  );
+};
+
+
+const resList = [
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "967051",
+    name: "McDonald's",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/9/6b40cfcf-8901-49c9-af50-f44066d319d4_967051.JPG",
+    locality: "Omaxe Mall",
+    areaName: "Chandni Chowk",
+    costForTwo: "₹400 for two",
+    cuisines: ["American", "Fast Food"],
+    avgRating: 4.3,
+    parentId: "630",
+    avgRatingString: "4.3",
+    totalRatingsString: "213",
+    sla: {
+      deliveryTime: 31,
+      lastMileTravel: 1.3,
+      serviceability: "SERVICEABLE",
+      slaString: "30-35 mins",
+      lastMileTravelString: "1.3 km",
+      iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+      nextCloseTime: "2025-07-19 23:00:00",
+      opened: true
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {}
+      }
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹39"
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {}
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {}
+      }
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--"
+      }
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  analytics: {
+    context: "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/delhi/mcdonalds-omaxe-mall-chandni-chowk-rest967051",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK"
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "804724",
+    name: "KFC",
+    cloudinaryImageId: "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/9dbea919-e4a4-4a05-9708-0476ad6dbf35_804724.JPG",
+    locality: "Kaccha Bagh",
+    areaName: "Chandni Chowk",
+    costForTwo: "₹400 for two",
+    cuisines: ["Burgers", "Rolls & Wraps", "Fast Food"],
+    avgRating: 4.2,
+    parentId: "547",
+    avgRatingString: "4.2",
+    totalRatingsString: "111",
+    sla: {
+      deliveryTime: 45,
+      lastMileTravel: 1.4,
+      serviceability: "SERVICEABLE",
+      slaString: "45-50 mins",
+      lastMileTravelString: "1.4 km",
+      iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+      nextCloseTime: "2025-07-19 23:59:00",
+      opened: true
+    },
+    badges: {
+      imageBadges: [
+        {
+          imageId: "Rxawards/_CATEGORY-Burger.png",
+          description: "Delivery!"
+        }
+      ]
+    },
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {
+          badgeObject: [
+            {
+              attributes: {
+                description: "Delivery!",
+                imageId: "Rxawards/_CATEGORY-Burger.png"
+              }
+            }
+          ]
+        },
+        textBased: {},
+        textExtendedBadges: {}
+      }
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹59"
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {}
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {}
+      }
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--"
+      }
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  analytics: {
+    context: "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/delhi/kfc-kaccha-bagh-chandni-chowk-rest804724",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK"
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "807783",
+    name: "Burger King",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/18/a049117e-1a3d-4ca0-94a0-9e25a17879df_807783.jpg",
+    locality: "Omaxe Mall",
+    areaName: "Chandni Chowk",
+    costForTwo: "₹350 for two",
+    cuisines: ["Burgers", "American"],
+    avgRating: 4.3,
+    parentId: "166",
+    avgRatingString: "4.3",
+    totalRatingsString: "681",
+    sla: {
+      deliveryTime: 35,
+      lastMileTravel: 1.7,
+      serviceability: "SERVICEABLE",
+      slaString: "35-40 mins",
+      lastMileTravelString: "1.7 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2025-07-19 23:59:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹59",
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {},
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--",
+      },
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+  },
+  analytics: {
+    context: "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb",
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/delhi/burger-king-omaxe-mall-chandni-chowk-rest807783",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK",
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+},
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  info: {
+    id: "350774",
+    name: "Subway",
+    cloudinaryImageId:
+      "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/12/6a3525b9-6858-461c-9905-8eaa490f7c8d_350774.jpg",
+    locality: "Satanam Road",
+    areaName: "Krishna Nagar",
+    costForTwo: "₹350 for two",
+    cuisines: ["sandwich", "Salads", "wrap", "Healthy Food"],
+    avgRating: 4.1,
+    parentId: "2",
+    avgRatingString: "4.1",
+    totalRatingsString: "4.2K+",
+    sla: {
+      deliveryTime: 28,
+      lastMileTravel: 5,
+      serviceability: "SERVICEABLE",
+      slaString: "25-30 mins",
+      lastMileTravelString: "5.0 km",
+      iconType: "ICON_TYPE_EMPTY"
+    },
+    availability: {
+      nextCloseTime: "2025-07-20 02:00:00",
+      opened: true
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {}
+      }
+    },
+    aggregatedDiscountInfoV3: {
+      header: "ITEMS",
+      subHeader: "AT ₹69"
+    },
+    orderabilityCommunication: {
+      title: {},
+      subTitle: {},
+      message: {},
+      customIcon: {}
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {}
+      }
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+    externalRatings: {
+      aggregatedRating: {
+        rating: "--"
+      }
+    },
+    ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  analytics: {
+    context: "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+  },
+  cta: {
+    link: "https://www.swiggy.com/city/delhi/subway-satanam-road-krishna-nagar-rest350774",
+    text: "RESTAURANT_MENU",
+    type: "WEBLINK"
+  },
+  widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  "info": {
+    "id": "836075",
+    "name": "California Burrito",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/3/6d078ea1-62dd-4016-a4cc-2a0f2edcd441_836075.jpg",
+    "locality": "Karol Bagh",
+    "areaName": "Karol Bagh",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+      "Mexican", "American", "Salads", "Continental",
+      "Keto", "Healthy Food", "Beverages", "Snacks",
+      "Desserts", "Fast Food"
+    ],
+    "avgRating": 4.5,
+    "parentId": "432183",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "2.7K+",
+    "sla": {
+      "deliveryTime": 36,
+      "lastMileTravel": 6.3,
+      "serviceability": "SERVICEABLE",
+      "slaString": "35-40 mins",
+      "lastMileTravelString": "6.3 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-07-20 03:00:00",
+      "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {},
+        "textBased": {},
+        "textExtendedBadges": {}
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "ITEMS",
+      "subHeader": "AT ₹129"
+    },
+    "orderabilityCommunication": {
+      "title": {},
+      "subTitle": {},
+      "message": {},
+      "customIcon": {}
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {},
+        "video": {}
+      }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "4.5",
+        "ratingCount": "137"
+      },
+      "source": "GOOGLE",
+      "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/delhi/california-burrito-karol-bagh-rest836075",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+  "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+  "info": {
+    "id": "659837",
+    "name": "Bakingo",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/7/10/666f50e7-670b-409b-9ccf-e8983056b43a_659837.jpg",
+    "locality": "Naiwala",
+    "areaName": "Karol Bagh",
+    "costForTwo": "₹299 for two",
+    "cuisines": ["Bakery", "Desserts", "Beverages", "Snacks"],
+    "avgRating": 4.6,
+    "parentId": "3818",
+    "avgRatingString": "4.6",
+    "totalRatingsString": "10K+",
+    "sla": {
+      "deliveryTime": 35,
+      "lastMileTravel": 6.2,
+      "serviceability": "SERVICEABLE",
+      "slaString": "35-40 mins",
+      "lastMileTravelString": "6.2 km",
+      "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+      "nextCloseTime": "2025-07-20 01:00:00",
+      "opened": true
+    },
+    "badges": {
+      "imageBadges": [
+        {
+          "imageId": "Rxawards/_CATEGORY-Desserts.png",
+          "description": "Delivery!"
+        }
+      ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+      "entityBadges": {
+        "imageBased": {
+          "badgeObject": [
+            {
+              "attributes": {
+                "description": "Delivery!",
+                "imageId": "Rxawards/_CATEGORY-Desserts.png"
+              }
+            }
+          ]
+        },
+        "textBased": {},
+        "textExtendedBadges": {}
+      }
+    },
+    "aggregatedDiscountInfoV3": {
+      "header": "ITEMS",
+      "subHeader": "AT ₹63"
+    },
+    "orderabilityCommunication": {
+      "title": {},
+      "subTitle": {},
+      "message": {},
+      "customIcon": {}
+    },
+    "differentiatedUi": {
+      "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      "differentiatedUiMediaDetails": {
+        "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+        "lottie": {},
+        "video": {}
+      }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+      "aggregatedRating": {
+        "rating": "--"
+      }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+  },
+  "analytics": {
+    "context": "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+  },
+  "cta": {
+    "link": "https://www.swiggy.com/city/delhi/bakingo-naiwala-karol-bagh-rest659837",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+  },
+  "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+},
+{
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "157768",
+      "name": "The Good Bowl",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/15/d15835f6-d21d-4308-985b-f34029eb9ee9_157768.jpg",
+      "locality": "Minto Road",
+      "areaName": "GANDHI MARKET",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "Biryani",
+        "Pastas",
+        "Punjabi",
+        "Desserts",
+        "Beverages"
+      ],
+      "avgRating": 4.3,
+      "parentId": "7918",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "4.5K+",
+      "sla": {
+        "deliveryTime": 36,
+        "lastMileTravel": 5.8,
+        "serviceability": "SERVICEABLE",
+        "slaString": "35-40 mins",
+        "lastMileTravelString": "5.8 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2025-07-19 23:59:00",
+        "opened": true
+      },
+      "badges": {
+        "textExtendedBadges": [
+          {
+            "iconId": "guiltfree/GF_Logo_android_3x",
+            "shortDescription": "options available",
+            "fontColor": "#7E808C"
+          }
+        ]
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {},
+          "textBased": {},
+          "textExtendedBadges": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "",
+                  "fontColor": "#7E808C",
+                  "iconId": "guiltfree/GF_Logo_android_3x",
+                  "shortDescription": "options available"
+                }
+              }
+            ]
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "ITEMS",
+        "subHeader": "AT ₹99"
+      },
+      "orderabilityCommunication": {
+        "title": {},
+        "subTitle": {},
+        "message": {},
+        "customIcon": {}
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {},
+          "video": {}
+        }
+      },
+      "reviewsSummary": {},
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {},
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-611f9070-9e28-47b1-92dc-0cdb7edbfadb"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/delhi/the-good-bowl-minto-road-gandhi-market-rest157768",
+      "text": "RESTAURANT_MENU",
+      "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+}
+];
+
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        { resList.map((restaurant) => (
+          <RestaurentCard key={restaurant.info.id} resData={restaurant.info}
+          />
+        )) }
+      </div>
+    </div>
+  );
+};
+
+
+
+const AppLayout = () => {
+  return (
+    <div className = "app">
+      <Header/>
+      <Body/>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const a = React.createElement("div",
-    {id:"parent"},
-    [React.createElement("div",
-        {id:"child"},
-        [React.createElement("h1",{},"i am hello"),React.createElement("h2",{},"i am hello2")]
-    ),React.createElement("div",
-        {id:"child"},
-        [React.createElement("h1",{},"i am hello"),React.createElement("h2",{},"i am hello2")]
-    )]
-)
 
-root.render(a)
-
-
+root.render(<AppLayout />)
